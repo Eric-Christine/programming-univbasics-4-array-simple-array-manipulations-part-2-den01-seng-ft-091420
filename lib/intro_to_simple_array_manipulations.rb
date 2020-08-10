02527546739c580221fa32d36ece3a586a7d02b4
Eric Christine <ericjchristine@gmail.com>
2020-08-10 20:05:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,19 +1,23 @@
 def using_concat(array1, array2)
   array1.concat(array2)
+  p array 
   p array1.concat(array2)
 end
 
 def using_insert(array, element)
   array.insert(4, element)
+  p array
   p array.insert(4, element)
 end
 
 def using_uniq(array)
   array.uniq
+  p array
   p array.uniq
 end
 
 def using_flatten(array)
   array.flatten
+  p array
   p array.flatten
 end
